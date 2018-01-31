a69742f4282ff49528aa98eed8aec70b18deea8a
Jason Chen <739077689@qq.com>
2018-01-31 10:08:13 +0800

    test

--- example.js
+++ example.js
@@ -1,8 +1,7 @@
 var hsl = require('./')
-var clockface = require('clockface');
+
 var hue = 133
 var saturation = 40
 var luminosity = 60
 var hex = hsl(hue, saturation, luminosity)
 console.log(hex) // #70c282
-console.dir(clockface)
